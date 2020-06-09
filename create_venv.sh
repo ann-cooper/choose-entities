21dbf3484e9cfb86c6e0baae8f09ab9934d9f3a9
ann-cooper <cooperannc@gmail.com>
2020-06-09 09:48:33 -0400

    Use poetry in venv creation and update requirements.txt

--- create_venv.sh
+++ create_venv.sh
@@ -2,5 +2,7 @@
 
 python3 -m venv myenv
 source myenv/bin/activate
-python3 -m pip install -r requirements.txt 
+python3 -m pip install poetry
+python3 -m poetry install
+# python3 -m pip install -r requirements.txt 
 python3 -m spacy download en
